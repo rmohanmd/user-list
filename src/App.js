671c4ddaa8b77b786e58444f795d7978621e0c19
rmohanmd <rmohan.md@gmail.com>
2022-10-14 00:51:58 -0400

    Added generic user list and display iot using map()

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React from "react";
 
+let userList = [
+  { id: 1, name: "Ro Mo", age: 35 },
+  { id: 2, name: "Ry Si", age: 33 },
+  { id: 3, name: "J U", age: 34 },
+];
 
 function App() {
   return (
     <div>
-
+      <ul>
+        {userList.map((user) => (
+          <li key={user.id} id={user.id}>
+            {user.name} {user.age}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
