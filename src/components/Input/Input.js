e93504079cb97817899b60bcac6f07c2b958830c
rmohanmd <rmohan.md@gmail.com>
2022-10-14 14:15:12 -0400

    Added input handler functions to update pieces of state

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,18 +1,35 @@
-import React from "react";
-
-const onSubmitHandler = (event) => {
-  event.preventDefault();
-  console.log("testing");
-};
+import React, { useState } from "react";
 
 const Input = () => {
+  const [newName, setNewName] = useState("");
+  const [newAge, setNewAge] = useState(0);
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log("testing");
+  };
+
+  const nameInputHandler = (event) => {
+    setNewName(event.target.value);
+    console.log(newName);
+  };
+
+  const ageInputHandler = (event) => {
+    setNewAge(event.target.value);
+  };
+
   return (
     <form onSubmit={onSubmitHandler}>
       <label>Name</label>
-      <input type="text" minLength="1" />
+      <input
+        value={newName}
+        type="text"
+        onChange={nameInputHandler}
+        minLength="1"
+      />
       <br />
       <label>Age</label>
-      <input type="number" min="0" />
+      <input value={newAge} type="number" onChange={ageInputHandler} min="0" />
       <button type="submit">Submit</button>
     </form>
   );
